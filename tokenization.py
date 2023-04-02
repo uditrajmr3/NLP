21552bdde7d2e5f6230a7580aaf387f8d7d11815
udit raj <uditraj3426@gmail.com>
2023-04-02 22:38:54 +0530

    tokenization

---
+++ tokenization.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+sentences = [
+    'Hello World',
+    'I love Programming',
+    'I love to code',
+    'This world is so dramatic'
+]
+
+tokenizer = Tokenizer(num_words = 100)
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
+print(word_index)
