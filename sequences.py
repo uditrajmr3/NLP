933a81b836b6ed088ef57440c052021b36a51a5a
udit raj <uditraj3426@gmail.com>
2023-04-03 00:52:23 +0530

    fixed imports

--- sequences.py
+++ sequences.py
@@ -1,6 +1,6 @@
 # from keras_preprocessing.sequence import pad_sequences
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.utils import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from keras_preprocessing.sequence import pad_sequences
 
 sentences = [
     'Hello World',
@@ -9,8 +9,13 @@ sentences = [
     'This world is so dramatic',
     'Do I really need to add a new sentence'
 ]
+test_data = [
+    'I added a new sentence.',
+    'Actually not, i added another sentence.',
+    'That means i added some sentences, not one'
+]
 
-tokenizer = Tokenizer(num_words = 100, oov_token='<OOV>')
+tokenizer = Tokenizer(num_words=100, oov_token='<OOV>')
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
 
@@ -20,4 +25,4 @@ padded = pad_sequences(sequences)
 # padded = pad_sequences(sequences, padding='post', truncating='post', maxlen=5) // with optional parameters
 print(word_index)
 print(sequences)
-print(word_index)
+print(padded)
--- tokenization.py
+++ tokenization.py
@@ -1,6 +1,4 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.preprocessing.text import Tokenizer
+from keras.preprocessing.text import Tokenizer
 
 sentences = [
     'Hello World',
@@ -12,4 +10,5 @@ sentences = [
 tokenizer = Tokenizer(num_words=100)
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
+
 print(word_index)
